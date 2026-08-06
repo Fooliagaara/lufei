23277ff9314804a39af37a259de0e91e1a79c713
Foolia <1215512085@qq.com>
2026-08-06 22:08:51 +0800

    水果类

---
+++ fruit.py
@@ -0,0 +1,19 @@
+class Fruit:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class Apple(Fruit):
+    def __init__(self, name, price):
+        super().__init__(name, price)
+        self.name = "apple"
+        self.price = price
+
+
+class Banana(Fruit):
+    def __init__(self, name, price):
+        super().__init__(name, price)
+        self.name = "banana"
+        self.price = price
+
